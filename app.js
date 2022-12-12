777f0386014fc4330dedb51c475b84a5bdc50203
Jennifer Zajac <zajac.je@northeastern.edu>
2022-12-12 03:11:03 -0500

    users register, login, logout, session

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import SessionController from './session-controller.js';
+import UsersController from './users/users-controller.js';
 
-const app = express()
-app.get('/', (req, res) => {res.send('Welcome to the meal plan server!')})
-app.listen(4000)
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/wecook'
+mongoose.connect(CONNECTION_STRING);
+
+const app = express();
+app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000' // TODO: change this to frontend env
+}))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // needs HTTPS
+}))
+app.use(express.json());
+app.get('/', (req, res) => {res.send('Welcome to the meal plan server!')});
+SessionController(app);
+UsersController(app);
+app.listen(4000);
